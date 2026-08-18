dc87a850759e6bcb7ecd3e9bffcef24929b2a9a2
manshika-aimaven <manshika.aimaven@gmail.com>
2026-08-18 12:54:33 +0530

    fix: serve .html extension for extensionless routes in server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,12 +2,10 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
-
 const outDir = path.join(__dirname, "out");
 
-app.use(express.static(outDir));
+app.use(express.static(outDir, { extensions: ["html"] }));
 
 app.get(/.*/, (req, res) => {
   res.sendFile(path.join(outDir, "index.html"));
