7e6ba041f4b25e6b5a1ce033d52ddbe11d22ae50
manshika-aimaven <manshika.aimaven@gmail.com>
2026-08-18 09:39:52 +0530

    Configure frontend for Railway deployment

---
+++ frontend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const outDir = path.join(__dirname, "out");
+
+app.use(express.static(outDir));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(outDir, "index.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Frontend running on port ${PORT}`);
+});
